e00546255ebeb50332b7d8045f6d68aec2e310e4
rknizzle <ryan@autotiv.com>
2019-01-11 12:43:50 -0500

    Added the ability to output time taken to a text file

--- performaceLogger.js
+++ performaceLogger.js
@@ -1,9 +1,18 @@
+const fs = require('fs')
+
 module.exports = {
   test: "testString", 
   createSession: function(label) {
     let session = Session(this, label)
     this.sessions.push(session)
     return session
+  },
+  clearFile: function() {
+    fs.writeFile("performanceLog.txt", "", (err) => {
+      if (err) {
+        throw new Error('Error clearing file: ' + err)
+      }
+    })
   },
   sessions: [],
   silent: false,
@@ -19,6 +28,7 @@ function Session(pl, labelInput) {
       label: null,
       start: function() {
         this.startTime = new Date()
+        return this
       },
       end: function() {
         this.endTime = new Date()
@@ -29,10 +39,22 @@ function Session(pl, labelInput) {
       },
       logOutput: function() {
         if(this.label) {
-          console.log(this.label + this.timeTakenInSeconds())
+          console.log(this.label + this.timeTakenInSeconds() + " seconds")
         }
         else {
-          console.log('' + this.timeTakenInSeconds())
+          console.log('' + this.timeTakenInSeconds() + " seconds")
+        }
+      },
+      logOutputToFile: function() {
+        if(this.label) {
+          fs.appendFile('performanceLog.txt', this.label + this.timeTakenInSeconds() + " seconds\n", (err) => {
+            if (err) {
+              throw new Error('Error while writing performance logs to file')
+            }
+          })
+        }
+        else {
+          fs.appendFile('performanceLog.txt',  this.timeTakenInSeconds() + " seconds\n")
         }
       },
       main: pl
